708fb3995ed3101870ab5b52aaef1e28ba565c5e
Chandra <30cpss@gmail.com>
2024-01-28 09:32:35 +0530

    setup up console project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SeeSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
